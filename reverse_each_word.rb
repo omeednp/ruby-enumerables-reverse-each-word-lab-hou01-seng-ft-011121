c1d642e36206e7787ec0aa24c72a1324f21faa40
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-06 08:49:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   
   sentence_array = sentence.split(' ')
   
-  sentence_array.collect do |word, reverse_sentence[]| 
+  sentence_array.collect do |word, reverse_sentence = []| 
     reverse_sentence << word.reverse
     return reverse_sentence.join(' ')
   end
